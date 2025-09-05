a7799ad97dd4aaae506ebb2b1b39bae2e8ba84cb
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2025-09-05 23:14:33 +0600

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,54 @@
 import React from 'react'
-import { FaSearch } from "react-icons/fa";
+import { FaSearch,FaUser, FaShoppingBag } from "react-icons/fa";
 import logo from "../assets/logo.png"
+import { Link } from 'react-router';
 
 
 const Navbar = () => {
+
+  const navItems = [
+    {title: "Jewelry & Accessories", path:"/"},
+    {title: "Clothing & Shoes", path:"/"},
+    {title: "Home & Living", path:"/"},
+    {title: "Wedding & Party", path:"/"},
+    {title: "Toys & Entertainment", path:"/"},
+    {title: "Art & Collectibles", path:"/"},
+    {title: "Craft Supplies & Tools", path:"/"},
+
+  ]
   return (
-    <header>
-      <nav>
+    <header className='max-w-screen-2xl xl:px-28 px-4 mx-auto '>
+      <nav className='flex justify-between items-center container md:py-4 pt-6 pb-3  '>
         <FaSearch />
         
         {/* logo */}
-        <a href=""> <img src={logo} alt="" /> </a>
+        <a href="/"> <img src={logo} alt="" /> </a>
+
+        {/* account and shopping btn */}
+         <div className='text-lg text-black sm:flex items-center gap-4 hidden'>
+           <a href="/" className='flex items-center gap-2'> <FaUser /> Account </a>
+           <a href="/" className='flex items-center gap-2'> <FaShoppingBag /> Shooping </a>
+         </div>
+
+
       </nav>
+
+      <hr />
+
+      {/* catagory items */}
+      <div>
+        <ul>
+          {
+            navItems.map(({title, path})=> (
+              <li key={title}>
+                <Link to="/">{title}</Link>
+
+              </li>
+             ) )
+          }
+        </ul>
+
+      </div>
     </header>
   )
 }
