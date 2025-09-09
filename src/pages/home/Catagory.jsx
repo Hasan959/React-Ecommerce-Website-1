14a74475bb4c9489c68c393733a180ccd1e63839
Hasan Mahmud <hasanmahmud.biddut@gmail.com>
2025-09-09 23:11:25 +0600

    five commit

--- src/pages/home/Catagory.jsx
+++ src/pages/home/Catagory.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Catagory = () => {
 
     const companyLogo = [
+     {id: 1, img:"/image/catagory/company/brand-1.png"},
+     {id: 2, img:"/image/catagory/company/brand-2.png"},
+     {id: 3, img:"/image/catagory/company/brand-3.png"},
+     {id: 4, img:"/image/catagory/company/brand-4.png"},
+     {id: 5, img:"/image/catagory/company/brand-5.png"},
+
         
     ]
 
@@ -10,10 +17,40 @@ const Catagory = () => {
     <div className='max-w-screen-2xl container xl:px-28 px-4 py-28'>
 
         {/* brand logo */}
-        <div>
-
+        <div className='flex items-center justify-around flex-wrap gap-4 py-5'>
+          {
+            companyLogo.map(({id,img})=>(
+              <div key={id}> <img src={img} alt="" /> </div>
+            ))
+          }
         </div>
          
+         {/* catagory grid */}
+         <div className='mt-8'>
+
+          <div className='font-semibold uppercase md:rotate-90 text-center bg-black text-white md:p-1.5 p-2 rounded-sm inline-flex'>
+          <p>Explore new and popular styles</p>
+         </div>
+         <div>
+          <Link to="/"> <img src="/image/image-1.png" alt="" /> </Link>
+         </div>
+
+         <div className='md:w-1/2'>
+          <div className='grid grid-cols-2 gap-2 '>
+            
+            <Link to="/"><img src="/image/image-2.png" alt="" /> </Link>
+            <Link to="/"><img src="/image/image-3.png" alt="" /></Link>
+            <Link to="/"><img src="/image/image-4.png" alt="" /></Link>
+            <Link to="/"><img src="/image/image-5.png" alt="" /></Link>
+
+          </div>
+         </div>
+
+         </div>
+
+
+
+
     </div>
   )
 }
